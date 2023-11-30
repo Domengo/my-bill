55c404eba66518397f2b6286603129d9cbc818d8
Domengo <dominicsengo@gmail.com>
2023-12-01 00:45:46 +0300

    deploy

--- src/app/api/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-
-const connectionString = 'postgres://dom:2252@localhost:5432/my_bill';
-
-// Create a PostgreSQL connection pool
-export default const pool = new Pool({
-    connectionString: connectionString,
-});
